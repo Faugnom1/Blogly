03678872ebc26348122c38f09c4065abd1d15b7e
Faugnom1 <128656243+Faugnom1@users.noreply.github.com>
2023-10-18 21:23:51 -0400

    Update models.py

--- models.py
+++ models.py
@@ -25,10 +25,6 @@ class User(db.Model):
 
 
 def connect_db(app):
-    """Connect this database to provided Flask app.
-
-    You should call this in your Flask app.
-    """
 
     db.app = app
     db.init_app(app)
